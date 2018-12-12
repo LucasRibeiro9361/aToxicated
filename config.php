85e0a8d48f26b2b1eb9886c96dfc3c3d2c0c68ac
unknown <danilodcp18@gmail.com>
2018-12-12 13:09:42 -0200

    c

--- config.php
+++ config.php
@@ -1,4 +1,6 @@
 <?php
+error_reporting(0);
+ini_set(“display_errors”, 0 );
 session_start();
 date_default_timezone_set('America/Sao_Paulo');
 $_SESSION['apikeylol'] = '?api_key=RGAPI-ad01800c-90b6-44a9-a2d3-5dd690a0fc33';
