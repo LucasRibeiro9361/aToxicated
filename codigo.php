b679188a1c55cd43ee8eb13ebf5abcaff71085e9
unknown <danilodcp18@gmail.com>
2018-12-12 09:40:41 -0200

    dan

--- codigo.php
+++ codigo.php
@@ -1,19 +1,12 @@
 <?php
 include 'config.php';
 include 'connect.php';
-include 'selectdedadosperfilparticular.php';
 include 'conviteperfil.php';
-if(isset($_SESSION["cdusuario"])){
-  include 'menulogado.php';
-}
-else{
-  header('Location: login.php');
-}
-error_reporting(0);
-ini_set(“display_errors”, 0 );
-?>
-<?php
+if (isset($_POST['nome']) && $_POST['nome'] ==! "") {
 $nom=$_POST['nome'];
 $_SESSION['equipe']=$nom;
-header('Location:perfilequipelolpublica.php')
+header('Location:perfilequipelolpublica.php');
+}else {
+  header('location:filtroequipeslol.php');
+}
 ?>
